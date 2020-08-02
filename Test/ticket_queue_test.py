d95592521f6e9918074ca1b1a3b51805f5b938e5
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-08-02 16:55:17 -0500

    I was getting my test right, so just made sure by breaking one of my test. It failed so now time to fix./

--- Test/ticket_queue_test.py
+++ Test/ticket_queue_test.py
@@ -38,6 +38,35 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(self.queue.queueSize(), 1)
         self.assertEqual(self.queue._isEmpty, False)
 
+    def test_remove_ticket(self):
+        """
+        Now we remove the ticket and the size should return to 0
+        and is empty should return to True
+        """
+        # add ticket first to be removed.
+        self.queue.addTicket(self.ticket1)
+        self.queue.removeTicket()
+        self.assertEqual(self.queue.queueSize(), 0)
+        self.assertEqual(self.queue._isEmpty, True)
+
+    def test_next_ticket(self):
+        """
+        To test this we will add 3 tickets.
+        First we test to see if next ticket returns ticket2 (Name: TicketName2)
+        Then we will test after next ticket two more times it should move back to 1
+        We will also test the current ticket, that indext should move along the ticket -1
+        ie. if current ticket is 1 then ticket name will be 2
+        """
+        self.queue.addTicket(self.ticket1)
+        self.queue.addTicket(self.ticket2)
+        self.queue.addTicket(self.ticket3)
+        # Since the queue starts at _current_ticket = 0
+        # that means we call next and it should = 1 and the ticket name should be ticketname2
+        self.queue.nextTicket()
+        # first check if the ticket is the samethen
+        self.assertEqual(self.queue.currentTicket(), self.ticket2)
+        self.assertEqual(self.queue._current_ticket, 2)
+
 
 if __name__ == '__main__':
     unittest.main()
