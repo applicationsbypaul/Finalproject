c13d71594bd2d10ee5e8b834c7453298735faa33
applicationsbypaul <32409508+applicationsbypaul@users.noreply.github.com>
2020-08-02 16:45:04 -0500

    Finished testing ahte add ticket method , I swear sometimes the this just breaks my test unit and I'm not sure why.

--- Test/ticket_queue_test.py
+++ Test/ticket_queue_test.py
@@ -9,13 +9,13 @@ date = datetime.now().replace(microsecond=0)
 
 
 class MyTestCase(unittest.TestCase):
-    def setUp(self) -> None:
+    def setUp(self):
         self.queue = TicketsQueue()
         self.ticket1 = Ticket('TicketName1', 'TicketDescription', '1235', 'Low', 'In Queue', date)
         self.ticket2 = Ticket('TicketName2', 'TicketDescription', '1235', 'Medium', 'In Queue', date)
         self.ticket3 = Ticket('TicketName3', 'TicketDescription', '1235', 'Emergency', 'In Queue', date)
 
-    def tearDown(self) -> None:
+    def tearDown(self):
         del self.queue
         del self.ticket1
         del self.ticket2
@@ -28,6 +28,15 @@ class MyTestCase(unittest.TestCase):
         """
         self.assertEqual(self.queue._current_ticket, 0)
         self.assertEqual(self.queue._isEmpty, True)
+
+    def test_add_ticket(self):
+        """
+        When adding a ticket we should get the list length to 1
+        and get isEmpty to False
+        """
+        self.queue.addTicket(self.ticket1)
+        self.assertEqual(self.queue.queueSize(), 1)
+        self.assertEqual(self.queue._isEmpty, False)
 
 
 if __name__ == '__main__':
